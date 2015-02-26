5be4a369236b21fadb88684f84e744260f3613d3
Zach Motsinger <zam2007@ku.edu>
2015-02-26 17:15:31 -0600

    added comments

--- quash.c
+++ quash.c
@@ -12,13 +12,15 @@ void prompt_user(char *arr_input[])
 	size_t bsize = 256;
 	char* str_input;
 
-	printf("[QUASH]$ ");
+	/* Get the input from the user */
+	printf("$ ");
 	str_input = (char *) malloc (bsize + 1);
 	bytes_in = getline(&str_input, &bsize, stdin);
 
 	if(bytes_in == -1)
 		printf("Error reading input.\n");
 
+	/* Tokenize the string and remove whitespace */
 	int i=0;
 	arr_input[i] = strtok(str_input," \n");
 
@@ -37,19 +39,26 @@ int main(int argc, char *argv[])
 		pid_t child_pid;
 		child_pid = fork();
 
-		//Parent process
+		/* Parent process */
 		if(child_pid!=0)
 		{
+			/* Wait for child to finish */
 			int status;
 			waitpid(child_pid, &status, 0);
+
+			/* Reset the input */
+			int i;
+			for(i = 0; i<100; i++)
+				input[i] = 0;
 
 			if(status == 0)
 				prompt_user(input);
 		}
 
-		//Child process
+		/* Child process */
 		if(child_pid==0)
 		{
+			/* Execute the command */
 			if(execvpe(input[0], input, environ) == -1)
 				printf("Error: command not on path.\n");
 			
