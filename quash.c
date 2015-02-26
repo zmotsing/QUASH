0840c85972eda702758766a165b4de8436343145
Zach Motsinger <zam2007@ku.edu>
2015-02-26 16:04:02 -0600

    working parent/child process

--- quash.c
+++ quash.c
@@ -2,31 +2,49 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
 
-int main(int argc, char *argv[])
+char* prompt_user()
 {
 	int bytes_in;
 	size_t bsize = 256;
 	char *input;
 
+	printf("[QUASH]$ ");
+	input = (char *) malloc (bsize + 1);
+	bytes_in = getline(&input, &bsize, stdin);
+
+	if(bytes_in == -1)
+		printf("Error reading input.\n");
+	else
+		input[strlen(input)-1] = 0;
+
+	return input;
+}
+
+int main(int argc, char *argv[])
+{
+	char* input;
+
+	input = prompt_user();
+
 	bool run = true;
 	while(run)
 	{
-		printf("[QUASH]$ ");
-		input = (char *) malloc (bsize + 1);
-		bytes_in = getline(&input, &bsize, stdin);
-	
-		if(bytes_in == -1)
-			printf("Error reading input.\n");
-		else
-			input[strlen(input)-1] = 0;
-
 		pid_t child_pid;
 		child_pid = fork();
 
+		//Parent process
 		if(child_pid!=0)
-			return child_pid;
-		else
+		{
+			int status;
+			waitpid(child_pid, &status, 0);
+
+			if(status == 0)
+				input = prompt_user();
+		}
+		//Child process
+		if(child_pid==0)
 		{
 			if(strcmp(input, "head") == 0)
 				execvp("head", argv);
@@ -43,9 +61,7 @@ int main(int argc, char *argv[])
 			else if(strcmp(input, "ls") == 0)
 				execvp("ls", argv);
 			else
-				run = false;
-
-			exit(0);
+				exit(0);
 		}
 	}
 	
